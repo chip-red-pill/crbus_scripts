649037f327e1439c257e96bf8ae75c1d0a3a0b2a
h0t_max <h0t_max@hotmail.com>
2020-10-13 17:48:47 +0300

    ldat write and cpuid patch functions added

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 **All information is provided for educational purposes only. Follow these instructions at your own risk. Neither the authors nor their employer are responsible for any direct or consequential damage or loss arising from any person or organization acting or failing to act on the basis of information contained in this page.**
 
 # Description
-IPC scripts allowing to extract [Intel Microcode](https://en.wikipedia.org/wiki/Intel_Microcode) (msrom) from your own Atom Goldmont platform.
+IPC scripts allowing to extract and modify [Intel Microcode](https://en.wikipedia.org/wiki/Intel_Microcode) (msrom) from your own Atom Goldmont platform.
+
+![screenshot](media/cpuid.jpg)
 
 # Research Team
 
--- ldat.py
+++ ldat.py
@@ -1,3 +1,6 @@
+#EXTREMELY UNSTABLE!!!
+#Please use it only if you understand what you are doing!!!
+
 def ldat_array_read(pdat_reg, array_sel, bank_sel, dword_idx, fast_addr):
     crbus_write(pdat_reg + 1, 0x10000 | ((dword_idx & 0xf) << 12) | ((array_sel & 0xf) << 8) | (bank_sel & 0xf))
     crbus_write(pdat_reg, 0xc00000 | (fast_addr & 0xffff))
@@ -41,3 +44,47 @@ def ms_match_patch_reg_write(addr, match_reg_val, patch_reg_val):
 
 def ms_patch_ram_write(addr, val):
     ldat_array_write(0x6a0, 4, 0, 0, addr, val)
+
+def ms_array_write_no_atomic(array_sel, bank_sel, dword_idx, fast_addr, val):
+    ldat_array_write(0x6a0, array_sel, bank_sel, dword_idx, fast_addr, val)
+
+def ms_patch_ram_write_no_atomic(addr, val):
+    ms_array_write_no_atomic(4, 0, 0, addr, val)
+
+def ms_match_patch_write_no_atomic(addr, val):
+    ms_array_write_no_atomic(3, 0, 0, addr, val)
+
+def ms_const_write_no_atomic(addr, val):
+    ms_array_write_no_atomic(2, 0, 0, addr, val)
+
+#Microcode CPUID[80000002] leaf patcher (uCode version: 0x40)
+def fix_cpuid_80000002():
+    ms_debug_defeature_val = crbus_read(0x38c)
+    crbus_write(0x38c, 0)
+    ms_match_patch_write_no_atomic(0x3e, 0x3e4221b5)
+    ms_match_patch_write_no_atomic(4, 0x3e4c21bd)
+    ms_patch_ram_write_no_atomic(0x21+0x80*0, 0xc008433e000b) # rax:= MOV_DSZ32(0x00006f43)
+    ms_patch_ram_write_no_atomic(0x21+0x80*1, 0x002410020220) # rax:= SHL_DSZ32(rax, 0x00000010)
+    ms_patch_ram_write_no_atomic(0x21+0x80*2, 0x80085b57700b) # tmp7:= MOV_DSZ32(0x0000755b)
+    ms_patch_ram_write_no_atomic(0x22+0x80*0, 0x0008204b808a) # tmp8:= MOV_DSZ32(0x00005220)
+    ms_patch_ram_write_no_atomic(0x22+0x80*1, 0x000100020de0) # rax:= OR_DSZ32(rax, tmp7)
+    ms_patch_ram_write_no_atomic(0x22+0x80*2, 0x002410023238) # rbx:= SHL_DSZ32(tmp8, 0x00000010)
+    ms_patch_ram_write_no_atomic(0x23+0x80*0, 0x00086417700b) # tmp7:= MOV_DSZ32(0x00006564)
+    ms_patch_ram_write_no_atomic(0x23+0x80*1, 0x000100023de3) # rbx:= OR_DSZ32(rbx, tmp7)
+    ms_patch_ram_write_no_atomic(0x23+0x80*2, 0x80082d43700a) # tmp7:= MOV_DSZ32(0x0000502d)
+    ms_patch_ram_write_no_atomic(0x24+0x80*0, 0x00086513800b) # tmp8:= MOV_DSZ32(0x00002055)
+    ms_patch_ram_write_no_atomic(0x24+0x80*1, 0x002410037237) # tmp7:= SHL_DSZ32(tmp7, 0x00000010)
+    ms_patch_ram_write_no_atomic(0x24+0x80*2, 0x800100021df8) # rcx:= OR_DSZ32(tmp8, tmp7)
+    ms_patch_ram_write_no_atomic(0x25+0x80*0, 0x40086933700b) # tmp7:= MOV_DSZ32(0x00006c69)
+    ms_patch_ram_write_no_atomic(0x25+0x80*1, 0x40086c76200a) # rdx:= MOV_DSZ32(0x00005d6c)
+    ms_patch_ram_write_no_atomic(0x25+0x80*2, 0x002410022222) # rdx:= SHL_DSZ32(rdx, 0x00000010)
+    ms_patch_ram_write_no_atomic(0x26+0x80*0, 0x000100022de2) # rdx:= OR_DSZ32(rdx, tmp7)
+    ms_patch_ram_write_no_atomic(0x26+0x80*1, 0)
+    ms_patch_ram_write_no_atomic(0x26+0x80*2, 0)
+    ms_const_write_no_atomic(0x21, 0x0000300000c0)
+    ms_const_write_no_atomic(0x22, 0x0000300000c0)
+    ms_const_write_no_atomic(0x23, 0x0000300000c0)
+    ms_const_write_no_atomic(0x24, 0x0000300000c0)
+    ms_const_write_no_atomic(0x25, 0x0000300000c0)
+    ms_const_write_no_atomic(0x26, 0x197ec80)
+    crbus_write(0x38c, ms_debug_defeature_val)
